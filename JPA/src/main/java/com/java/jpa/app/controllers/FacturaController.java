6e11a6c1184b007a8d6bd19e70e847b89145a6a6
Domingo Mendoza <nashi.amaterasu@gmail.com>
2021-03-05 10:27:11 -0600

    Factura_4
    vista

--- JPA/src/main/java/com/java/jpa/app/controllers/FacturaController.java
+++ JPA/src/main/java/com/java/jpa/app/controllers/FacturaController.java
@@ -6,23 +6,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.java.jpa.app.models.entity.Cliente;
+import com.java.jpa.app.models.entity.Factura;
 import com.java.jpa.app.services.IClienteService;
 
 @Controller
-@RequestMapping(name = "/factura/**")
+@RequestMapping("/factura")
+@SessionAttributes("facturas")
 public class FacturaController {
 
 	@Autowired
 	private IClienteService clienteService;
 	
-//	@GetMapping("/form/{clienteId}")
-//	public String crear(@PathVariable(value = "clienteId") Long clienteId, Model model,
-//			RedirectAttributes flash) {
-//		Cliente cliente = clienteService.findById(clienteId);
-//		
-//		return "factura/form";
-//	}
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Model model,
+			RedirectAttributes flash) {
+		Cliente cliente = clienteService.findById(clienteId);
+		if(cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe");
+			return "redirect:/listar";
+		}
+		
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+		
+		model.addAttribute("factura", factura);
+		model.addAttribute("titulo", "crear factura");
+		
+		return "factura/form";
+	}
 }
