bd10cec03b6ac90b913ee4b581ea19c58488a7ec
Domingo Mendoza <nashi.amaterasu@gmail.com>
2021-03-10 14:57:42 -0600

    Roles de usuario
    Creacion de instancias para mapear las tablas de usuario y roles, relaciones e instancias, configuracion de Spring security para tomar el servicio de configuracion

--- JPA/src/main/java/com/java/jpa/app/SpringSecurityConfig.java
+++ JPA/src/main/java/com/java/jpa/app/SpringSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.java.jpa.app;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +8,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.java.jpa.app.auth.handler.LoginSuccessHandler;
+import com.java.jpa.app.services.JpaUserDetailService;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
@@ -35,11 +36,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 	
-	@Autowired
-	private DataSource dataSource;
+//	@Autowired
+//	private DataSource dataSource;
 	
 	@Autowired
 	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private JpaUserDetailService userService;
 	
 	 @Autowired
 	 public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
@@ -49,13 +53,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 //		 builder.inMemoryAuthentication()
 //		 .withUser(users.username("admin").password("sa").roles("ADMIN", "USER"))
 //		 .withUser(users.username("amat").password("sa").roles("USER"));
-//		 Autenticacion con JDBC
-		 builder.jdbcAuthentication()
-		 .dataSource(dataSource)
-		 .passwordEncoder(passwordEncoder)
-		 .usersByUsernameQuery("select username, password, enabled from users where username=?")
-		 .authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?")
-		 ;
+		 
+//		 Autenticacion con JDBC 2-forma
+//		 builder.jdbcAuthentication()
+//		 .dataSource(dataSource)
+//		 .passwordEncoder(passwordEncoder)
+//		 .usersByUsernameQuery("select username, password, enabled from users where username=?")
+//		 .authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?")
+//		 ;
+		 
+//		 3era forma
+		 builder.userDetailsService(userService)
+		 .passwordEncoder(passwordEncoder);
+		 
 	 }
 
 	@Override
---
+++ JPA/src/main/java/com/java/jpa/app/models/dao/IUsuarioDao.java
@@ -0,0 +1,10 @@
+package com.java.jpa.app.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.java.jpa.app.models.entity.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
+
+	public Usuario findByUsername(String username);
+}
