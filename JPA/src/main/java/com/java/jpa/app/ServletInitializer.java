05a7c9efe33da3feccb080be4f8c295922f7fbee
Domingo Mendoza <nashi.amaterasu@gmail.com>
2021-03-12 19:03:54 -0600

    Configuracion Tomcat
    Se añaden las configuraciones para desplegar la app en Tomcat externo y se actualizan las url en jquery

---
+++ JPA/src/main/java/com/java/jpa/app/ServletInitializer.java
@@ -0,0 +1,18 @@
+package com.java.jpa.app;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+/**
+ * Archivo necesario para desplegar en tomcat externo
+ * @author amaterasu1906
+ *
+ */
+public class ServletInitializer extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JpaApplication.class);
+	}
+
+}
