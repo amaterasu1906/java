be7dd96a7d51569f5ddc747abff789f31a8b5c3e
Domingo Mendoza <nashi.amaterasu@gmail.com>
2021-03-16 12:39:50 -0600

    x
    x

--- reactor/src/main/java/com/reactor/app/ReactorApplication.java
+++ reactor/src/main/java/com/reactor/app/ReactorApplication.java
@@ -1,13 +1,27 @@
 package com.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ReactorApplication {
+import reactor.core.publisher.Flux;
 
+@SpringBootApplication
+public class ReactorApplication implements CommandLineRunner{
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	public static void main(String[] args) {
 		SpringApplication.run(ReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Kokomi","Wendy","Haruhi")
+				.doOnNext( elemento -> System.out.println(elemento));
+		nombres.subscribe(logger::info);
+		
 	}
 
 }
