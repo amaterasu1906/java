f109f95a7674950949c11c2e09938b1acb52d8b6
Domingo Mendoza <nashi.amaterasu@gmail.com>
2021-03-23 22:56:56 -0600

    Test
    test y variantes, csv, filesource, method, etc

--- junit/src/test/java/com/junit/app/ejemplos/models/CuentaTest.java
+++ junit/src/test/java/com/junit/app/ejemplos/models/CuentaTest.java
@@ -1,23 +1,79 @@
 package com.junit.app.ejemplos.models;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.junit.app.ejemplos.exceptions.DineroInsuficienteException;
 
+//Se maneja solo una instancia, no paralelo
+//@TestInstance(Lifecycle.PER_CLASS )
 class CuentaTest {
+	Cuenta cuenta;
+	@BeforeEach
+	void initMethodTest() {
+		this.cuenta = new Cuenta("Kokomi", new BigDecimal("10.01"));
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("Finalizando");
+	}
+	
+//	Se ejecuta solo 1 vez para inicializar
+	@BeforeAll
+	static void beforeall() {
+		System.out.println("Inicializando.");
+	}
+//	Se ejecuta solo una vez al final del test
+	@AfterAll
+	static void afterAll() {
+		System.out.println("Finalizando el test");
+	}
 
 	@Test
+	@DisplayName("Probando creacion de cuenta")
 	void test() {
-		Cuenta cuenta = new Cuenta("Kokomi", new BigDecimal("10.01"));
-		assertEquals("Kokomi", cuenta.getPersona());
+		assertEquals("Kokomi", this.cuenta.getPersona());
 		
 	}
 	
 	@Test
+	@Disabled
 	void testSaldo() {
 		Cuenta c = new Cuenta("Kokomi", new BigDecimal("10.01"));
 		assertEquals(10.01, c.getSaldo().doubleValue());
@@ -98,15 +154,169 @@ class CuentaTest {
 		}, () -> {
 			assertTrue(banco.getCuentas().stream().anyMatch(e -> e.getPersona().equals("Kokomi")));
 		});
+	}
+	@DisplayName("Sistemas Operativos.")
+	@Nested
+	class SistemaOperativoTest{
+//		Habilitar pruebas solo para windows
+		@Test
+		@EnabledOnOs(OS.WINDOWS)
+		void testSoloWindows() {
+			
+		}
 		
+//		Deshabilitar prueba para windows
+		@Test
+		@DisabledOnOs(OS.WINDOWS)
+		void testSinWindows() {
+			
+		}
+		@Test
+		@EnabledOnOs({OS.LINUX, OS.MAC})
+		void testLinuxMac() {
+			
+		}
 		
+	}
+	@Nested
+	class JavaVersionTest{
+		@EnabledOnJre(JRE.JAVA_8)
+		@Test
+		void soloJDK8() {
+			
+		}
+		@DisabledOnJre(JRE.JAVA_8)
+		@Test
+		void disableJDK() {
+			
+		}
+		@Test
+		@Disabled
+		void systemProperties() {
+			Properties p = System.getProperties();
+			p.forEach( (k, v) -> {
+				System.out.println(k + " - " + v);
+			});
+		}
 		
+		@Test
+		@EnabledIfSystemProperty(named = "java.version", matches = "15.0.2")
+		void  testJavaVersion() {
+			
+		}
+	}
+	@Nested
+	class SystemPropertiesTest{
+//	solo para desarrollo
+		@Test
+		@EnabledIfSystemProperty(named = "ENV", matches = "dev")
+		void testDev() {		
+		}
 		
+		@Test
+		void printVarEnviroment() {
+			Map<String, String> getenv = System.getenv();
+//			getenv.forEach( (k, v) -> System.out.println(k + " - " + v));
+		}
 		
+		@Test
+		@EnabledIfEnvironmentVariable(named = "JAVA_HOME", matches = ".*jdk-15.0.*")
+		void testJavaHome() {
+			
+		}
 		
+		@Test
+		@EnabledIfEnvironmentVariable( named = "NUMBER_OF_PROCESSORS", matches = "12")
+		void testProcesadores() {
+			
+		}
 		
+		@Test
+		@EnabledIfEnvironmentVariable(named = "ENV", matches = "prod")
+		@DisplayName("ENVIROMENT")
+		void testEnv() {
+			
+		}
 		
-		
-		
+		@Test
+		void testDevSaldo() {
+			boolean esDev = "prod".equals(System.getProperty("ENV"));
+//		Se ejecuta si pasa el primer test, si no se deshabilita
+			assumeTrue(esDev);
+			assertEquals(10.01, cuenta.getSaldo().doubleValue());
+		}
+		@Test
+		void testDevSaldo2() {
+			boolean esDev = "prod".equals(System.getProperty("ENV"));
+//		solo se ejecuta lo de adentro si se cumple la premisa
+			assumingThat(esDev, () -> {
+				assertEquals(10.01, cuenta.getSaldo().doubleValue());			
+			});
+		}
+	}
+	
+
+	@DisplayName("Repeticiones")
+	@RepeatedTest(value = 3, name = "{displayName} Repeticion #{currentRepetition} de {totalRepetitions}")
+	void testDebitoCuentaRepeat(RepetitionInfo info) {
+		if( info.getCurrentRepetition() == 3) {
+			System.out.println("Repeticion: " + info.getCurrentRepetition());
+		}
+		Cuenta c = new Cuenta("Kokomi", new BigDecimal("10.01"));
+		c.debito(new BigDecimal("1"));
+		assertNotNull(c.getSaldo(), () -> "El saldo no puede estar nula");
+		assertEquals(9, c.getSaldo().intValue());
+		assertEquals("9.01", c.getSaldo().toPlainString());
+	}
+	@ParameterizedTest(name = "numero {index} ejecutando con valor {0} - {argumentsWithNames}")
+	@ValueSource(strings = {"1", "2", "3", "4", "5", "10.01"})
+	void testDebitoCuentaValueSource(String monto) {
+		Cuenta c = new Cuenta("Kokomi", new BigDecimal("10.01"));
+		c.debito(new BigDecimal(monto));
+		assertNotNull(c.getSaldo(), () -> "El saldo no puede estar nula");
+		assertTrue(c.getSaldo().compareTo(BigDecimal.ZERO) >= 0);
+	}
+	
+	@Disabled
+	@ParameterizedTest(name = "numero {index} ejecutando con valor {0} - {argumentsWithNames}")
+	@CsvSource({"1,1", "2, 2.1", "3, 10.00"})
+	void testDebitoCuentaCsv(String index, String monto) {
+		System.out.println("["+index+"] Monto: "+ monto);
+		Cuenta c = new Cuenta("Kokomi", new BigDecimal("10.01"));
+		c.debito(new BigDecimal(monto));
+		assertNotNull(c.getSaldo(), () -> "El saldo no puede estar nula");
+		assertTrue(c.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+	}
+	
+	@ParameterizedTest(name = "numero {index} ejecutando con valor {0} - {argumentsWithNames}")
+	@CsvFileSource(resources = "/data.csv")
+	void testDebitoCuentaCsvFileSource(String monto) {
+		System.out.println(" Monto: "+ monto);
+		Cuenta c = new Cuenta("Kokomi", new BigDecimal("10.01"));
+		c.debito(new BigDecimal(monto));
+		assertNotNull(c.getSaldo(), () -> "El saldo no puede estar nula");
+		assertTrue(c.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+	}
+	
+	@ParameterizedTest(name = "numero {index} ejecutando con valor {0} - {argumentsWithNames}")
+	@MethodSource("montoList")
+	void testDebitoCuentaMethodSource(String monto) {
+		Cuenta c = new Cuenta("Kokomi", new BigDecimal("10.01"));
+		c.debito(new BigDecimal(monto));
+		assertNotNull(c.getSaldo(), () -> "El saldo no puede estar nula");
+		assertTrue(c.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+	}
+	static List<String> montoList(){
+		return Arrays.asList("1", "2", "3", "4", "5", "10.00");
+	}
+	
+	@ParameterizedTest(name = "numero {index} ejecutando con valor {0} - {argumentsWithNames}")
+	@CsvSource({"2,1", "3, 2.1", "3, 10.00", "10.01, 10.01"})
+	void testDebitoCuentaCsv2(String saldo, String monto) {
+		System.out.println("["+saldo+"] Monto: "+ monto);
+		Cuenta c = new Cuenta("Kokomi", new BigDecimal(saldo));
+		c.debito(new BigDecimal(monto));
+		assertNotNull(c.getSaldo(), () -> "El saldo no puede estar nula");
+		assertTrue(c.getSaldo().compareTo(BigDecimal.ZERO) > 0);
 	}
 }
