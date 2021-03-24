f00197864888a16be7d4890d269c41fbf4fd154a
Domingo Mendoza <nashi.amaterasu@gmail.com>
2021-03-24 15:03:34 -0600

    Mockito
    creacion proyecto mockito con dependencias
    Mockito-core, Mockito-jupiter, junit-jupiter

---
+++ mockito/src/test/java/com/mockito/app/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockito.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
